a5300d6ea84310c6e6afda3c7d3315bb01924e9c
Senne <Senne.mollez@student.vives.be>
2021-03-01 22:13:33 +0100

    model verplaatst van APP naar DATA

---
+++ PlantenApplicatie.DATA/Models/CommensalismeMulti.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlantenApplicatie.DATA.Models
+{
+    public partial class CommensalismeMulti
+    {
+        public long Id { get; set; }
+        public long PlantId { get; set; }
+        public string Eigenschap { get; set; }
+        public string Waarde { get; set; }
+
+        public virtual Plant Plant { get; set; }
+    }
+}
