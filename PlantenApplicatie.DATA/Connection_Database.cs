2d41ec8b1d1e62ca7648b9a8ba5209b924548919
Senne <Senne.mollez@student.vives.be>
2021-03-04 09:07:28 +0100

    Connection_Database aanmaken

---
+++ PlantenApplicatie.DATA/Connection_Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantenApplicatie.DATA
+{
+    public class Connection_Database
+    {
+        public static string CONNECTION_STRING = "Server=desktop-2r9tir4\\sqlexpress;Database=Planten2021;Trusted_Connection=True";
+    }
+}
