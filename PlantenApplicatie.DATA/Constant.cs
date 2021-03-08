b8de59cdaffeb5ee7a110cada3d8c20bf0f42877
Senne <Senne.mollez@student.vives.be>
2021-03-08 10:13:25 +0100

    .

--- PlantenApplicatie.DATA/Constant.cs
+++ PlantenApplicatie.DATA/Constant.cs
@@ -13,7 +13,7 @@ namespace PlantenApplicatie.DATA
         //public static string CONNECTION_STRING = "Server=desktop-2r9tir4\\sqlexpress;Database=Planten2021;Trusted_Connection=True";
 
         //Maarten
-        public static string CONNECTION_STRING = "Server=laptop-2de86bg5\\vives;Database=Planten2021;Trusted_Connection=True";
+        //public static string CONNECTION_STRING = "Server=laptop-2de86bg5\\vives;Database=Planten2021;Trusted_Connection=True";
 
         //Stephanie
         //public static string CONNECTION_STRING = "Server=laptop-8dek2824\\vives;Database=Planten2021;Trusted_Connection=True";
