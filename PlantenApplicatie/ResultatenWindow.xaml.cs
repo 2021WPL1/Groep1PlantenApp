941bd5d1cddcd2fca1a47e6369aea45dbc2bf876
Stephanie <stephanie.bracke@student.vives.be>
2021-03-11 11:07:16 +0100

    New wpf

---
+++ PlantenApplicatie/ResultatenWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PlantenApplicatie
+{
+    /// <summary>
+    /// Interaction logic for ResultatenWindow.xaml
+    /// </summary>
+    public partial class ResultatenWindow : Window
+    {
+        public ResultatenWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
