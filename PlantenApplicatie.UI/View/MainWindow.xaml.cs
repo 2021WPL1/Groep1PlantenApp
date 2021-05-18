f12ccde4588060b6ca39f1ba9237345b5c1b1f55
Senne <Senne.mollez@student.vives.be>
2021-05-18 15:23:17 +0200

    naam fout fix

--- PlantenApplicatie.UI/View/MainWindow.xaml.cs
+++ PlantenApplicatie.UI/View/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.EntityFrameworkCore;
 using PlantenApplicatie.Data;
-using PlantenApplicatie.UI.ViewModels;
+using PlantenApplicatie.UI.ViewModel;
 
 namespace PlantenApplicatie.UI.View
 {
--- PlantenApplicatie.UI/ViewModel/MainViewModel.cs
+++ PlantenApplicatie.UI/ViewModel/MainViewModel.cs
@@ -4,8 +4,9 @@ using System.Collections.ObjectModel;
 using System.Text;
 using PlantenApplicatie.Data;
 using PlantenApplicatie.Domain.Models;
+using PlantenApplicatie.UI.ViewModel;
 
-namespace PlantenApplicatie.UI.ViewModels
+namespace PlantenApplicatie.UI.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
