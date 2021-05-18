f70a3ad55d71aca6806a2958afd8ac87b7b62045
Senne <Senne.mollez@student.vives.be>
2021-05-18 15:22:16 +0200

    fix

--- PlantenApplicatie.UI/ViewModel/ViewModelBase.cs
+++ PlantenApplicatie.UI/ViewModel/ViewModelBase.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace PlantenApplicatie.UI.ViewModels
+namespace PlantenApplicatie.UI.ViewModel
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
