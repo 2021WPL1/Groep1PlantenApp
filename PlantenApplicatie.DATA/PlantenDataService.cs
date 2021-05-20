097aead7ad044bcfcebb0ca9e3468a8500bbc489
Stephanie <stephanie.bracke@student.vives.be>
2021-05-20 11:47:15 +0200

    Alle details weergeven van een plant

---
+++ PlantenApplicatie.DATA/PlantenDataService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlantenApplicatie.Data;
+using PlantenApplicatie.Domain.Models;
+
+namespace PlantenApplicatie.Data
+{
+    public class PlantenDataService
+    {
+        private static readonly PlantenDataService instance = new PlantenDataService();
+        private Planten2021Context context;
+
+        public static PlantenDataService Instance()
+        {
+            return instance;
+        }
+
+        private PlantenDataService()
+        {
+            this.context = new Planten2021Context();
+        }
+
+        public List<TfgsvType> GetTfgsvTypes()
+        {
+            return context.TfgsvType.ToList();
+        }
+        public List<TfgsvFamilie> GetTfgsvFamilies()
+        {
+            return context.TfgsvFamilie.ToList();
+        }
+        public List<TfgsvGeslacht> GetTfgsvGeslachten()
+        {
+            return context.TfgsvGeslacht.ToList();
+        }
+        public List<TfgsvSoort> GetTfgsvSoorten()
+        {
+            return context.TfgsvSoort.ToList();
+        }
+        public List<TfgsvVariant> GetTfgsvVarianten()
+        {
+            return context.TfgsvVariant.ToList();
+        }
+
+        public Object[] GetFilteredFamilies(long typeId)
+        {
+            Object[] fgsv = new object[4];
+            fgsv[0] = context.TfgsvFamilie.Where(p => p.TypeTypeid == typeId).ToList();
+            fgsv[1] = context.TfgsvGeslacht.Where(p => p.FamilieFamile.TypeTypeid == typeId).ToList();
+            fgsv[2] = context.TfgsvSoort.Where(p => p.GeslachtGeslacht.FamilieFamile.TypeTypeid == typeId).ToList();
+
+            //Bug variant heeft geen soort om te koppelen
+            fgsv[3] = context.TfgsvVariant.ToList();
+
+            return fgsv;
+        }
+
+        public Object[] GetFilteredGeslachten(long familieId)
+        {
+            Object[] gsv = new object[3];
+            gsv[0] = context.TfgsvGeslacht.Where(p => p.FamilieFamileId == familieId).ToList();
+            gsv[1] = context.TfgsvSoort.Where(p => p.GeslachtGeslacht.FamilieFamileId == familieId).ToList();
+
+            //Bug variant heeft geen soort om te koppelen
+            gsv[2] = context.TfgsvVariant.ToList();
+
+            return gsv;
+        }
+
+        public Object[] GetFilteredSoorten(long geslachtId)
+        {
+            Object[] sv = new object[2];
+            sv[0] = context.TfgsvSoort.Where(p => p.GeslachtGeslachtId == geslachtId).ToList();
+
+            //Bug variant heeft geen soort om te koppelen
+            sv[1] = context.TfgsvVariant.ToList();
+
+            return sv;
+        }
+
+        public List<TfgsvVariant> GetFilteredVarianten(long soortId)
+        {
+            //Bug variant heeft geen soort om te koppelen
+            return context.TfgsvVariant.ToList();
+        }
+        //Geeft de plant en al zijn informatie
+        public Plant GetPlantWithId(long Id)
+        {
+            return context.Plant.SingleOrDefault(p => p.PlantId == Id);
+        }
+        //Geeft de plant en zijn uiterlijke kenmerken + habitus
+        public Fenotype GetFenotype(long Id)
+        {
+            return context.Fenotype.SingleOrDefault(f => f.PlantId == Id);
+        }
+        //Geeft de plant en zijn behoeftes
+        public Abiotiek GetAbiotiek(long Id)
+        {
+            return context.Abiotiek.SingleOrDefault(a => a.PlantId == Id);
+        }
+        //Geeft de plant en zijn ontwikkelsnelheid & strategie
+        public Commensalisme GetCommensalisme(long Id)
+        {
+            return context.Commensalisme.SingleOrDefault(c => c.PlantId == Id);
+        }
+        //Geeft de plant & zijn specifieke eigenschappen
+        public ExtraEigenschap GetExtraEigenschap(long Id)
+        {
+            return context.ExtraEigenschap.SingleOrDefault(e => e.PlantId == Id);
+        }
+        //Geeft de plant en de het onnderhoud per maand
+        public BeheerMaand GetBeheerMaand(long Id)
+        {
+            return context.BeheerMaand.SingleOrDefault(b => b.PlantId == Id);
+        }
+    }
+}
