dd5a902cb5d5694d09efb51367a452f10ffa936c
32472 <32472@LAPTOP-V1OVHLTU>
2021-05-20 09:29:26 +0200

    Edit 6

--- PlantenApplicatie.UI/ViewModel/ResultaatViewModel.cs
+++ PlantenApplicatie.UI/ViewModel/ResultaatViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using PlantenApplicatie.Data;
 using PlantenApplicatie.Domain.Models;
@@ -14,52 +15,9 @@ namespace PlantenApplicatie.UI.ViewModel
         {
             PlantenResultaat = new Plant();
         }
-
-        //Stephanie en Maarten
-        //Hiermee krijg je een plant terug adhv zijn Id.
-        /*public Plant GetPlantWithId(long Id)
-        {
-            return context.Plant.SingleOrDefault(p => p.PlantId == Id);
-        }*/
         public void fillLabels(Plant plant)
         {
             PlantenResultaat = plant;
-            /*bool enter = false;
-            for (int i = 0; i < plant.Fgsv.Length; i++)
-            {
-                string letter = plant.Fgsv.Substring(i, 1);
-                if (letter == " " && i >= 25 && enter == false)
-                {
-                    enter = true;
-                }
-                else
-                {
-                    if (enter != true)
-                    {
-                        lblLatinName.Content += letter;
-                    }
-                    else
-                    {
-                        lblLatinName2.Content += letter;
-                    }
-                }
-            }
-            if (plant.Type != null)
-            {
-                lblType.Content = plant.Type;
-            }
-            else
-            {
-                lblType.Content = " ";
-            }
-
-            lblType.Content = plant.Type;
-            lblFamily.Content = plant.Familie;
-            lblGeslacht.Content = plant.Geslacht;
-            lblSoort.Content = plant.Soort;
-            lblVariant.Content = plant.Variant;
-            lblPlantdichtheidMax.Content = plant.PlantdichtheidMax;
-            lblPlantdichtheidMin.Content = plant.PlantdichtheidMin;*/
         }
     }
 }
