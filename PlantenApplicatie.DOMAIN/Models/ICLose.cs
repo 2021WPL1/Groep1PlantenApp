594416b212915dee63248c1d7954eb116b2df8b2
r0780533 <79833337+r0780533@users.noreply.github.com>
2021-05-25 08:57:54 +0200

    GUI aangepast

---
+++ PlantenApplicatie.DOMAIN/Models/ICLose.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantenApplicatie.Domain.Models
+{
+ public    interface ICLose
+    {
+        void Close();
+    }
+}
