568f1d7763e39f41f408cee927f70c5a84aff005
Senne <Senne.mollez@student.vives.be>
2021-05-26 09:46:30 +0200

    deel 2

--- PlantenApplicatie.Data/PlantenDataService.cs
+++ PlantenApplicatie.Data/PlantenDataService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using PlantenApplicatie.Data;
-using PlantenApplicatie.UI.Models;
+using PlantenApplicatie.Domain.Models;
 
 namespace PlantenApplicatie.Data
 {
