601e2d6a19a4ce2de21d7985c6fb5c1ec231e984
Stephanie <stephanie.bracke@student.vives.be>
2021-05-27 14:39:32 +0200

    Scaffold & Namespace (update2)

---
+++ PlantenApplicatie.DOMAIN/Models/BeheerDaden.cs
@@ -0,0 +1,8 @@
+﻿namespace PlantenApplicatie.Domain.Models
+{
+    public partial class BeheerDaden
+    {
+        public int Id { get; set; }
+        public string Beheerdaad { get; set; }
+    }
+}
