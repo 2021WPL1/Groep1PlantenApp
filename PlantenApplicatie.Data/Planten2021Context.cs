e782758080aca4962d39099229e4b3902f00c2e4
Senne <Senne.mollez@student.vives.be>
2021-06-03 09:52:17 +0200

    crash fix

--- PlantenApplicatie.Data/Planten2021Context.cs
+++ PlantenApplicatie.Data/Planten2021Context.cs
@@ -60,7 +60,7 @@ namespace PlantenApplicatie.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(Constant.CONNECTION_STRING);
+                optionsBuilder.EnableSensitiveDataLogging().UseSqlServer(Constant.CONNECTION_STRING);
             }
         }
 
